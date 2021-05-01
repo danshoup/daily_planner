0d60ba3ea4b06006abe2396d0a4a7c32b50fdfc9
Daniel Shoup <79939129+danshoup@users.noreply.github.com>
2021-05-01 19:13:11 -0400

    added time blocks and save buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,3 @@
 // Use Moment.js to format today's date for the jumbotron
 var today = moment();
-$("#currentDay").text(today.format("dddd, MMMM Mo"));
+$("#currentDay").text(today.format("dddd, MMMM Mo - YYYY"));
